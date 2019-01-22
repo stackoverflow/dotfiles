186168fa6955a2f6917cbbf5b7b24a29c5310dcd
Islon Scherer <islon.scherer@rewe-digital.com>
2019-01-22 15:46:01 +0100

    some improvements

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -8,8 +8,8 @@ if [ ! -f $tmpbg ]; then
     #convert $1 -scale 10% -scale 1000% -gamma 0.5 $tmpbg
     #convert $1 -fill black -colorize 50%  $tmpbg
     #blur
-    convert $1 -fill black -colorize 50% -filter Gaussian -blur 0x2 $tmpbg
+    convert $WALLPAPER -fill black -colorize 50% -filter Gaussian -blur 0x2 $tmpbg
     convert $tmpbg $icon -gravity center -composite -matte $tmpbg
 fi
 
-i3lock -i $tmpbg
+i3lock -c 000000 -i $tmpbg
